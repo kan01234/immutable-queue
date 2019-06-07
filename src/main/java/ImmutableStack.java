2d414cbd7074e1b69e57532976da33b09439ff44
kan01234 <safghjkl@gmail.com>
2019-06-07 22:04:05 +0800

    init commit

---
+++ src/main/java/ImmutableStack.java
@@ -0,0 +1,24 @@
+
+public class ImmutableStack<T> implements Stack<T> {
+
+  public T head() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean isEmpty() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public Stack<T> push(T t) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Stack<T> pop() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
