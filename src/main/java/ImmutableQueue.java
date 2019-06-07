b4d8d951e71f086d8a48304f88c90de32c3d770a
kan01234 <safghjkl@gmail.com>
2019-06-07 21:48:20 +0800

    init commit

---
+++ src/main/java/ImmutableQueue.java
@@ -0,0 +1,24 @@
+
+public class ImmutableQueue<T> implements Queue<T> {
+
+  public Queue<T> enQueue(T t) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Queue<T> deQueue() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public T head() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean isEmpty() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
