2b0b102e08335e5d1c919e33bd205f34603dcad4
kan01234 <safghjkl@gmail.com>
2019-06-08 14:27:41 +0800

    implement ImmutableQueue

--- src/main/java/ImmutableQueue.java
+++ src/main/java/ImmutableQueue.java
@@ -1,24 +1,39 @@
 
-public class ImmutableQueue<T> implements Queue<T> {
+public final class ImmutableQueue<T> implements Queue<T> {
+
+  private final Stack<T> stack;
+
+  public ImmutableQueue() {
+    stack = new ImmutableStack<T>();
+  }
+
+  public ImmutableQueue(Stack<T> stack) {
+    this.stack = stack;
+  }
 
   public Queue<T> enQueue(T t) {
-    // TODO Auto-generated method stub
-    return null;
+    return new ImmutableQueue<T>(stack.push(t));
   }
 
   public Queue<T> deQueue() {
-    // TODO Auto-generated method stub
-    return null;
+    return new ImmutableQueue<T>(reserveStack(reserveStack(stack).pop()));
   }
 
   public T head() {
-    // TODO Auto-generated method stub
-    return null;
+    return (T) reserveStack(stack).head();
   }
 
   public boolean isEmpty() {
-    // TODO Auto-generated method stub
-    return false;
+    return stack == null;
+  }
+
+  private final Stack<T> reserveStack(Stack<T> stack) {
+    Stack<T> reserveStack = new ImmutableStack<T>();
+    while (!stack.isEmpty()) {
+      reserveStack = reserveStack.push(stack.head());
+      stack = stack.pop();
+    }
+    return reserveStack;
   }
 
 }
