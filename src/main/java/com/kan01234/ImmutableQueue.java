6ff2faa13f2b20c1f7efcefdebde044dc4be0d4d
kan01234 <safghjkl@gmail.com>
2019-06-11 22:25:54 +0800

    add java doc

--- src/main/java/com/kan01234/ImmutableQueue.java
+++ src/main/java/com/kan01234/ImmutableQueue.java
@@ -6,15 +6,29 @@ public final class ImmutableQueue<T> implements Queue<T> {
 
   private final Stack<T> reserve;
 
+  /**
+   * Constructor to create ImmutableQueue
+   * 
+   * @param order data ordered in FIFO
+   * @param reserve data ordered reserve in FIFO
+   */
   public ImmutableQueue(Stack<T> order, Stack<T> reserve) {
     this.order = order;
     this.reserve = reserve;
   }
 
+  /**
+   * @param t data need to added to queue
+   * @return return new ImmutableQueue
+   **/
   public Queue<T> enQueue(T t) {
     return new ImmutableQueue<T>(order, reserve.push(t));
   }
 
+  /**
+   * Method to remove data in Immutable
+   * @return return new ImmutableQueue, EmptyInstance if the queue is empty
+   */
   public Queue<T> deQueue() {
     if (order.isEmpty()) {
       Stack<T> o = reserveStack(reserve).pop();
@@ -28,6 +42,10 @@ public final class ImmutableQueue<T> implements Queue<T> {
       return new ImmutableQueue<T>(o, reserve);
   }
 
+  /**
+   * get head of the queue
+   * @return return head data of the queue
+   */
   public T head() {
     if (isEmpty())
       return null;
@@ -37,14 +55,26 @@ public final class ImmutableQueue<T> implements Queue<T> {
       return order.head();
   }
 
+  /**
+   * check queue is empty or not
+   * @return true is empty, otherwise false
+   */
   public boolean isEmpty() {
     return false;
   }
 
+  /**
+   * get the empty instance
+   * @return get a empty instance
+   */
   public static final Queue getEmptyInstance() {
     return EmptyInstance.getInstance();
   }
 
+  /**
+   * @param stack stack to reserve
+   * @return reserved stack
+   */
   private static final Stack reserveStack(Stack stack) {
     Stack reserveStack = ImmutableStack.getEmptyInstance();
     while (!stack.isEmpty()) {
@@ -58,23 +88,43 @@ public final class ImmutableQueue<T> implements Queue<T> {
 
     private static final EmptyInstance emptyInstance = new EmptyInstance();
 
+    /**
+     * return emptyInstance
+     * @return return singleton emptyInstance
+     */
     public static final EmptyInstance getInstance() {
       return emptyInstance;
     }
 
+    /**
+     * add data to queue
+     * @return return new ImmtuableQueue
+     */
     public Queue<T> enQueue(T t) {
       return new ImmutableQueue<T>(ImmutableStack.getEmptyInstance().push(t),
           ImmutableStack.getEmptyInstance());
     }
 
+    /**
+     * remove data from queue, return empty instance since removing data from empty queue
+     * @return emptyInstance
+     */
     public Queue<T> deQueue() {
       return emptyInstance;
     }
 
+    /**
+     * return first data from queue, return null since get data from empty queue
+     * @return null
+     */
     public T head() {
       return null;
     }
 
+    /**
+     * check queue is empty, always true
+     * @return true
+     */
     public boolean isEmpty() {
       return true;
     }
